6347c267597d807c6b879549d02f809dc36b5117
mschoe <michael.schoettes@camunda.com>
2014-10-22 11:14:03 +0200

    blame

---
+++ scripttask/xslt-scripttask/src/main/java/org/camunda/bpm/example/xsltexample/XsltApplication.java
@@ -0,0 +1,9 @@
+package org.camunda.bpm.example.xsltexample;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("XSLT Example")
+public class XsltApplication extends ServletProcessApplication {
+  // empty implementation
+}
